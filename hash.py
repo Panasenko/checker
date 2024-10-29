32dc055e2ec0bd2cbbe534de8b26abcdb7baf50e
Maks <panasenko>
2024-10-29 20:49:01 +0200

    Удалены лишние файлы

--- hash.py
+++
@@ -1,7 +0,0 @@
-import re
-def validate_hashes(hash_value):
-    sha256_pattern = r"\b([a-fA-F0-9]{32}|[a-fA-F0-9]{40}|[a-fA-F0-9]{64})\b"
-    return bool(re.fullmatch(sha256_pattern, hash_value))
-
-
-print(validate_hashes("634438a50ae1990c4f8636801c41046"))   
