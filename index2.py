5fac24a52093de67e623e51fd1140147cda63b12
Maksim Panasenko <panasenkomaksim@gmail.com>
2024-11-28 23:13:07 +0200

    Добавлен класс Таск и Процессинг

--- index2.py
+++ index2.py
@@ -15,6 +15,7 @@ from rich import print
 from rich.table import Table
 from rich.style import Style
 
+from typing import Union
 #TODO: Расставить логинки по всем методам
 load_dotenv()
 logging.basicConfig(level=logging.ERROR)
@@ -32,38 +33,47 @@ def main(input_file: typer.FileText = typer.Argument(None, help="Входной 
     Raises:
     None
     """
+
+
     if input_file:
-        scheduler(input_file.read())
+        Processor.main(input_file.read())
     elif not sys.stdin.isatty():
-        scheduler(sys.stdin.read())
+        Processor.main(sys.stdin.read())
     else:
         typer.echo("Не предоставлено ни файла, ни данных через stdin.")
 
-def scheduler(content: str):
-    array_requests_objects = []
-    array_novalid_objects = []
 
-    for line in content.splitlines():
-        
-        indicator_object = Indicators(line)
 
-        if indicator_object.get_status_valid():
-            request = RequestBuilder(indicator_object)
-            request_object = request.get_object()
 
-            if request_object is not None:
-                array_requests_objects.append(request_object)
-        else:
-            array_novalid_objects.append(indicator_object)
+    # if input_file:
+    #     scheduler(input_file.read())
+    # elif not sys.stdin.isatty():
+    #     scheduler(sys.stdin.read())
+    # else:
+    #     typer.echo("Не предоставлено ни файла, ни данных через stdin.")
 
-    call_api = CallAPI(array_requests_objects)
-    asyncio.run(call_api.caller())
-
-    print(call_api.__dict__)
-
-class Task:
-    pass
-
+# def scheduler(content: str):
+#     array_requests_objects = []
+#     array_novalid_objects = []
+#
+#     for line in content.splitlines():
+#
+#         indicator_object = Indicators(line)
+#
+#         if indicator_object.status_valid:
+#             request = RequestBuilder(indicator_object)
+#             request_object = request.get_object()
+#
+#             if request_object is not None:
+#                 array_requests_objects.append(request_object)
+#         else:
+#             array_novalid_objects.append(indicator_object)
+#
+#     call_api = CallAPI(array_requests_objects)
+#     asyncio.run(call_api.caller())
+#
+#     print(call_api.__dict__)
+#
 
 class Indicators:
     IP_PATTERN = r'^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$'
@@ -74,23 +84,34 @@ class Indicators:
         self.indicator = indicator
         self._status_valid = False
         self._type_indicator: str
-
         self.valid_indicators(self.indicator)
 
+    """
+    Обьевление геттеров и сеттеров
+    """
+
+    @property
     def get_indicator(self) -> str:
         return self.indicator
 
-    def get_status_valid(self) -> bool:
+    @property
+    def status_valid(self) -> bool:
         return self._status_valid
 
-    def set_status_valid(self, value: bool):
+    @status_valid.setter
+    def status_valid(self, value: bool):
         self._status_valid = value
 
-    def get_type_indicator(self) -> str:
+    @property
+    def type_indicator(self) -> str:
         return self._type_indicator
 
     def set_type_indicator(self, value: str):
         self._type_indicator = value
+
+    """
+    Блок валидаций индикаторов компрометации
+    """
 
     def _validate_ip_adress(self, value) -> bool:
         return bool(re.fullmatch(self.IP_PATTERN, value))
@@ -103,31 +124,25 @@ class Indicators:
 
     def valid_indicators(self, option: str):
         if self._validate_ip_adress(option):
-            self.set_status_valid(True)
+            self.status_valid = True
             self.set_type_indicator("ip_address")
 
         elif self._validate_hashes(option):
-            self.set_status_valid(True)
+            self.status_valid = True
             self.set_type_indicator("hash_file")
 
         elif self._validate_domain(option):
-            self.set_status_valid(True)
+            self.status_valid = True
             self.set_type_indicator("domain")
         else:
             self.set_type_indicator("no_valid")
 
+"""
+Создание запроса в сервис ViruseTotal
+"""
 class RequestBuilder:
         def __init__(self, indicator: Indicators) -> None:
             self.indicator = indicator
-
-        def get_object(self):
-            try:
-                request_object = self.RequestFactory.create_request(self.indicator)
-            except ValueError as e:
-                logging.error(f"Создание обьекта класса RequestFactory заверщилось ошибкой: {e}. Обработка индикатора {self.indicator.get_indicator()}")
-                return None
-            else:
-                return request_object
 
         class RequestVirusTotal(ABC):
             BASE_URL_VT = os.getenv("BASE_URL_VT")
@@ -135,7 +150,6 @@ class RequestBuilder:
             
             def __init__(self, indicator: str) -> None:
                 self.indicator = indicator
-                self._response: dict
 
             @property
             @abstractmethod
@@ -147,36 +161,15 @@ class RequestBuilder:
             def get_header(self) -> dict:
                 pass
 
-            def set_response(self, response: dict, fields: list):
-                dict_response = {}
-                if bool(response and fields):
-                    for item in fields:
-                        if item == "last_analysis_stats":
-                            dict_response.update(response.pop('last_analysis_stats'))
-                        else:
-                            dict_response[item] = response[item]
-                self._response = dict_response
-
             @property
             @abstractmethod
             def get_fields(self) -> list:
                 pass
 
-            @property
-            def get_response(self)-> dict:
-                return self._response
-
         class RequestHash(RequestVirusTotal):
             def __init__(self, indicator: str) -> None:
                 super().__init__(indicator) 
                 self.url = f"{super().BASE_URL_VT}files/{self.indicator}" 
-
-            def set_response(self, response: dict, fields: list):
-                super().set_response(response, fields)
-
-            @property
-            def get_response(self)-> dict:
-                return self._response
 
             @property
             def get_header(self) -> dict:
@@ -195,13 +188,6 @@ class RequestBuilder:
                 super().__init__(indicator)
                 self.url = f"{super().BASE_URL_VT}ip_addresses/{self.indicator}"
 
-            def set_response(self, response: dict, fields: list):
-                super().set_response(response, fields)
-
-            @property
-            def get_response(self)-> dict:
-                return self._response
-
             @property
             def get_header (self) -> dict:
                 return super().HEADER
@@ -219,13 +205,6 @@ class RequestBuilder:
                 super().__init__(indicator)
                 self.url = f"{super().BASE_URL_VT}domains/{self.indicator}"
 
-            def set_response(self, response: dict, fields: list):
-                super().set_response(response, fields)
-
-            @property
-            def get_response(self)-> dict:
-                return self._response
-
             @property
             def get_header(self) -> dict:
                 return super().HEADER
@@ -238,59 +217,158 @@ class RequestBuilder:
             def get_fields(self) -> list:
                 return ['last_analysis_stats','last_dns_records_date','whois','whois_date','creation_date', 'last_update_date','last_modification_date']
 
+        def get_object(self) -> Union[RequestHash, RequestIPAdress, RequestDomain, None]:
+            try:
+                request_object = self.RequestFactory.create_request(self.indicator)
+            except ValueError as e:
+                logging.error(f"Создание обьекта класса RequestFactory заверщилось ошибкой: {e}. Обработка индикатора {self.indicator.get_indicator}")
+                return None
+            else:
+                return request_object
+
         class RequestFactory:
             @staticmethod
             def create_request(indicator: Indicators):
-                if indicator.get_type_indicator() == "hash_file":
-                    return RequestBuilder.RequestHash(indicator.get_indicator())
+                if indicator.type_indicator == "hash_file":
+                    return RequestBuilder.RequestHash(indicator.get_indicator)
 
-                elif indicator.get_type_indicator() == "ip_address":
-                    return RequestBuilder.RequestIPAdress(indicator.get_indicator())
+                elif indicator.type_indicator == "ip_address":
+                    return RequestBuilder.RequestIPAdress(indicator.get_indicator)
 
-                elif indicator.get_type_indicator() == "domain":
-                    return RequestBuilder.RequestDomain(indicator.get_indicator())
+                elif indicator.type_indicator == "domain":
+                    return RequestBuilder.RequestDomain(indicator.get_indicator)
                 else:
                     raise ValueError("Unknown type of Indicators")
 
+class Task:
+    def __init__(self, ioc) -> None:
+        self._ioc = ioc
+        self._indicator: Indicators
+        self._request: Union[RequestBuilder.RequestHash, RequestBuilder.RequestIPAdress, RequestBuilder.RequestDomain, None]
+        self._response: dict
+    
+    @property
+    def indicator(self) -> Indicators:
+        return self._indicator
+
+    @indicator.setter
+    def indicator(self, indicator: Indicators) -> None:
+        self._indicator = indicator
+
+    @property
+    def request(self) -> Union[RequestBuilder.RequestHash, RequestBuilder.RequestIPAdress, RequestBuilder.RequestDomain, None]:
+        return self._request
+
+    @request.setter
+    def request(self, request: Union[RequestBuilder.RequestHash, RequestBuilder.RequestIPAdress, RequestBuilder.RequestDomain, None] ) -> None:
+        self._request = request
+
+    @property
+    def response(self) -> dict:
+        return self._response
+
+    @response.setter
+    def response(self, response: dict) -> None:
+        self._response = response
+
+class Processor:
+    @staticmethod
+    def main(content: str):
+        tasks = Processor.conveyor(content)
+        Processor.call_api(tasks["valid"])
+
+    @staticmethod
+    def conveyor(content: str) -> dict:
+
+        valid_objects = []
+        novalid_objects = []
+
+        for line in content.splitlines():
+            """Создание обьекта задачи"""
+            task = Task(list)
+
+            """Валидация индикаторов компрометации"""
+            task.indicator = Indicators(line)
+
+            if task.indicator.status_valid:
+                task.request = RequestBuilder(task.indicator).get_object()
+                valid_objects.append(task)
+            else:
+                novalid_objects.append(task)
+        return {
+            "valid": valid_objects,
+            "no_valid": novalid_objects
+        }
+                
+    @staticmethod
+    def call_api(valid_tasks: list):
+        call_api = CallAPI(valid_tasks)
+        res = asyncio.run(call_api.caller())
+        print(res[1].response)
+
 class CallAPI:
-    def __init__(self, request_obj: list) -> None:
-        self.request_obj = request_obj
+    def __init__(self, tasks: list) -> None:
+        self.tasks = tasks
         self.result_lst: list
+
 
     def get_result(self) -> list:
         return self.result_lst
 
-    async def fetch(self, obj: RequestBuilder.RequestVirusTotal, results: list, fields: list):
+    async def fetch(self, task: Task, results: list, fields: list):
         try:
             async with aiohttp.ClientSession() as session:
-                async with session.get(obj.get_url, headers=obj.get_header) as response:
-                    if response.status == 200:
-                        data = await response.json()
-                        res_attr = data["data"]["attributes"]
-                        obj.set_response(res_attr, fields)
-                        results.append(obj.get_response)
-                    else:
-                        print(f"Ошибка: {response.status}")
+                if task.request:
+                    async with session.get(task.request.get_url, headers=task.request.get_header) as response:
+                        if response.status == 200:
+                            data = await response.json()
+                            res_attr = data["data"]["attributes"]
+                            task.response = self.set_response(res_attr, fields)
+                            results.append(task)
+                        else:
+                            print(f"Ошибка: {response.status}")
+                else:
+                    print("В обьекте Task отсутствует сформированный обьект зазпрос")
         except aiohttp.ClientError as e:
             print(f"Client error: {e}")
         except asyncio.TimeoutError:
             print("Запрос занял слишком много времени")
 
+    def set_response(self, response: dict, fields: list) -> dict:
+        dict_response = {}
+        if bool(response and fields):
+            for item in fields:
+                if item == "last_analysis_stats":
+                    dict_response.update(response.pop('last_analysis_stats'))
+                else:
+                    dict_response[item] = response[item]
+        return dict_response
+
+    # async def caller(self):
+    #     results = []
+    #     for task in self.tasks:
+    #         await self.fetch(task, results, task.request.get_fields)
+    #     self.result_lst = results
+
     async def caller(self):
         results = []
-        for obj in self.request_obj:
-            await self.fetch(obj, results, obj.get_fields)
-        self.result_lst = results
+        for task in self.tasks:
+            await self.fetch(task, results, task.request.get_fields)
+        return results
+
 
 class ReportBuilder:
     def __init__(self, result_lst: CallAPI, novalid_lst: list) -> None:
         self.result_lst = result_lst
         self.novalid_lst = novalid_lst
 
+    def convert_date(self, timestamp):
+        value = datetime.datetime.fromtimestamp(timestamp)
+        return value.strftime('%d %B %Y')
 
-    def build_table(self, res_lst: list):
-        for item in res_lst:
-           print() 
+    # def build_table(self, res_lst: list):
+    #     for item in res_lst:
+    #        print() 
 
     def print_table(self, table: Table):
         print(table)
@@ -320,9 +398,6 @@ class ReportBuilder:
         def add_table_row(self, data):
             type_tag, malicious, suspicious, sha256, last_submission_date, last_modification_date = data
             super().add_row(type_tag, malicious, suspicious, sha256, last_submission_date, last_modification_date)
-
-
-
 
 
 if __name__ == "__main__":
